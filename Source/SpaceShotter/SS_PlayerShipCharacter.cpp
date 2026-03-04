c935116157f4044a395ddc0204eda3f13e3f7628
danielminarro <120184889+DanielMinarroBernardo@users.noreply.github.com>
2026-03-04 12:06:43 +0100

    Clase 04/03

---
+++ Source/SpaceShotter/SS_PlayerShipCharacter.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SS_PlayerShipCharacter.h"
+
+// Sets default values
+ASS_PlayerShipCharacter::ASS_PlayerShipCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	ShipMesh = CreateDefaultSubobject<UStaticMeshComponent>((TEXT("ShipMesh")));
+	ShipMesh->SetupAttachment(GetMesh());
+	//FirePoint = CreateDefaultSubobject<USceneComponent>((TEXT("FirePoint")));
+}
+
+// Called when the game starts or when spawned
+void ASS_PlayerShipCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASS_PlayerShipCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+}
+
+// Called to bind functionality to input
+void ASS_PlayerShipCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
